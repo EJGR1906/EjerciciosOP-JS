d31cefdc8f96dfb0064887acefb3d8326ab528d1
EJGR1906 <110251679+EJGR1906@users.noreply.github.com>
2022-12-14 16:58:44 -0400

    Add files via upload

---
+++ 02-Sintaxis/ejercicio-2.js
@@ -0,0 +1,17 @@
+//Variables
+const nombre = "Eduardo";
+const edad = 21; 
+const desarrollador = true;
+const fecha_nacimiento = new Date(2001, 5, 19);
+const libro = {
+    titulo: "",
+    autor: "",
+    fecha: "",
+    url: "",
+}
+
+//Ejercicio: creamos la lista
+const lista = [nombre, edad, desarrollador, fecha_nacimiento, libro]
+
+//Mostramos en la consola
+console.log(lista)
