bc6e4a933074157a79f997c439f8ec4beea4f7f2
EJGR1906 <110251679+EJGR1906@users.noreply.github.com>
2023-01-17 11:53:06 -0400

    Add files via upload

---
+++ 08-funciones/funciones.js
@@ -0,0 +1,29 @@
+const funcion_sin_parametros = () => true;
+console.log(funcion_sin_parametros());
+
+
+
+function* autoid() {
+  let id = 0;
+  while (true) {
+    id++;
+    if (id === 10) {
+      return id;
+    }
+    yield id;
+  }
+}
+
+const id = autoid()
+
+console.log(id.next().value)
+console.log(id.next().value)
+console.log(id.next().value)
+console.log(id.next().value)
+console.log(id.next().value)
+console.log(id.next().value)
+console.log(id.next().value)
+console.log(id.next().value)
+console.log(id.next().value)
+console.log(id.next().value)
+console.log(id.next().value)
