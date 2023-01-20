8b8be85b42d8cedc5423d10fa8d5cec1128ad451
EJGR1906 <eduardogonzalez1333@gmail.com>
2023-01-20 14:11:10 -0400

    commit

---
+++ 15-Persistencia/index.js
@@ -0,0 +1,12 @@
+const nombre = "Eduardo";
+const apellido = "Gonzalez";
+const dos_min = new Date(2023, 0, 20, 17, 40);
+
+const miObjeto = {
+    nombre: nombre,
+    apellido: apellido,
+};
+
+// localStorage.setItem("miObjeto", JSON.stringify(miObjeto));
+// sessionStorage.setItem("miObjeto", JSON.stringify(miObjeto));
+// document.cookie = "CookieEduardo = SoyUnaCookie; expires=" + dos_min;
